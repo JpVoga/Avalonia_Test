b7ce744c1b660442fabce41ce6ebca62274aec6b
JpVoga <jpvoga@gmail.com>
2025-02-12 16:24:22 -0300

    .

--- MainView.axaml.cs
+++ MainView.axaml.cs
@@ -1,7 +1,6 @@
 using System;
 using Avalonia.Controls;
-using Avalonia.Media.Imaging;
-using Avalonia.Platform;
+using Avalonia.Input;
 
 namespace Avalonia_Test;
 
@@ -10,5 +9,12 @@ public partial class MainView: Window
     public MainView()
     {
         InitializeComponent();
+    }
+
+    private void OnSideMenuImageClicked(object sender, PointerPressedEventArgs args) {
+        if (args.ClickCount >= 2) {
+            // Double click!
+            // Stopped on video 3, at 3:00
+        }
     }
 }
--- viewModels/MainViewModel.cs
+++ viewModels/MainViewModel.cs
@@ -1,8 +1,22 @@
+using Avalonia.Svg.Skia;
 using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace Avalonia_Test;
 
 public partial class MainViewModel: ViewModelBase {
-    [ObservableProperty()] // Class has to be partial for this!
-    private string test = "hello";
+
+    // Seems to be how you observe properties
+    /*private string _testProp = "prop 0";
+    public string TestProp {
+        get => _testProp;
+        set {
+            SetProperty(ref _testProp, value);
+        }
+    }*/
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(SideMenuImage))] // Update binding value when this changes
+    private bool sideMenuExpanded = true;
+
+    public SvgImage SideMenuImage => new SvgImage{Source = SvgSource.Load($"avares://{nameof(Avalonia_Test)}/assets/images/{(SideMenuExpanded? "logo":"icon")}.svg")};
 }
